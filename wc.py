8b19086e75675cbf7597d9759bad2eff21d5e3c0
Brian Sahota <64430701+scratchytube@users.noreply.github.com>
2026-04-13 13:20:55 -0400

    count bytes functionality completed

--- wc.py
+++ wc.py
@@ -1,1 +1,20 @@
-# initial commit
+import sys
+import argparse
+
+
+def count(data: bytes) -> int:
+    return len(data)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "-bytes", action="store_true", help="Count bytes")
+    flags = parser.parse_args()
+    
+    data = sys.stdin.buffer.read()
+    
+    if flags.bytes:
+        print(count(data))
+        
+if __name__ == "__main__":
+    main()
